3987b6953287e76682b22ec66d1d6d2508835cb9
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-09-27 23:16:04 -0300

    Cria controller com actions padrão

---
+++ app/controllers/api/v1/contacts_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::ContactsController < Api::V1::ApiController
+  before_action :set_contact, only: [:show, :update, :destroy]
+  before_action :require_authorization!, only: [:show, :update, :destroy]
+
+  def index
+    @contacts = current_user.contacts
+
+    render json: @contacts
+  end
+
+  def show
+    render json: @contact
+  end
+
+  def create
+    @contact = Contact.new(contact_params.merge(user: current_user))
+    if @contact.update(contact_params)
+      render json: @contact
+    else
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @contact.update(contact_params)
+      render json: @contact
+    else
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact.destroy
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :phone, :description)
+  end
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { message: e.message }, status: :not_found
+  end
+end
